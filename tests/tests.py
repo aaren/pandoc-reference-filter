4ddc0637491f8b7c5d5ec669b444be9fd40c403d
Aaron O'Leary <aaron.oleary@gmail.com>
2014-09-26 17:18:51 +0100

    complete tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,11 +1,53 @@
+import subprocess
+
+
+def call_pandoc(format):
+    pandoc_cmd = ('pandoc', 'spec.md',
+                  '--filter', 'internal-references.py',
+                  '--to', format)
+    p = subprocess.Popen(pandoc_cmd, stdout=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    return stdout
+
+
+def _test(format):
+    pandoc_output = call_pandoc(format)
+
+    ref_file = 'tests/spec.{ext}'.format(ext=format)
+
+    with open(ref_file) as f:
+        return pandoc_output, f.read()
+
+
 def test_markdown():
-    pass
+    pandoc_output, ref = _test('markdown')
+    assert(pandoc_output == ref)
+
 
 def test_html():
-    pass
+    pandoc_output, ref = _test('html')
+    assert(pandoc_output == ref)
+
 
 def test_html5():
-    pass
+    pandoc_output, ref = _test('html5')
+    assert(pandoc_output == ref)
+
 
 def test_latex():
-    pass
+    pandoc_output, ref = _test('latex')
+    assert(pandoc_output == ref)
+
+
+def test_all_formats():
+    for format in ('markdown', 'latex', 'html', 'html5'):
+        _test(format)
+
+
+if __name__ == '__main__':
+    print "Comparing pandoc output with reference output in tests/spec.format"
+    test_markdown()
+    test_html()
+    test_html5()
+    test_latex()
+    print "All comparison tests passed ok!"
