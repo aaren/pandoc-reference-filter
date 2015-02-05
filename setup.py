2d61ad616d9564c347b96530488891e9cf3333b9
Aaron O'Leary <aaron.oleary@gmail.com>
2015-02-05 16:04:41 +0000

    version bump and update setup.py. Fixes #6.

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 from setuptools import setup
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError):
-    long_description = ''
 
 setup(
     name="pandoc-internal-references",
     version='0.4',
     description="Image attributes and internal referencing in markdown",
-    long_description=long_description,
     packages=['internalreferences'],
     author="Aaron O'Leary",
     author_email='dev@aaren.me',
     license='BSD 2-Clause',
-    url='http://github.com/aaren/pandoc-internal-references',
+    url='https://github.com/aaren/pandoc-reference-filter',
     install_requires=['pandocfilters', 'pandoc-attributes'],
     entry_points={
         'console_scripts': [
